20db46d6cc12431b159f102aaf94b33e784ca05d
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-14 11:42:37 +0200

    Forms required fields test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('checks required fields', () => {
+	const { getAllByTestId } = render(<App />);
+	const inputArr = getAllByTestId('input');
+	expect(inputArr[0]).not.toBeRequired();
+	expect(inputArr[3]).toBeRequired();
+	expect(inputArr.length).toBe(4);
+
+	// const
 });
