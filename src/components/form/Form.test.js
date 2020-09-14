13ae7f76d82442816699c1a74742a19c22ca0581
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-14 14:44:50 +0200

    input onChange event test

--- src/components/form/Form.test.js
+++ src/components/form/Form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import Form from './Form';
 import Input from './Form';
 import '@testing-library/jest-dom';
@@ -30,4 +30,35 @@ test('Submit button disabled when form is invalid', () => {
 	const component = render(<Form {...props} />);
 	const button = component.container.querySelector('button');
 	expect(button).toBeDisabled;
+});
+
+test('test input onChange event', () => {
+	const props = {
+		form: {
+			formFields: {
+				name: {
+					basic: {
+						name: 'test name',
+						placeholder: 'test placeholder'
+					},
+					type: 'input',
+					value: '',
+					validation: {
+						required: true,
+						needToCheck: false,
+						isEmail: false
+					},
+					isValid: false,
+					touched: false
+				}
+			},
+			formValid: false
+		}
+	};
+	const onChangeMock = jest.fn();
+	const component = render(<Input onChange={onChangeMock} {...props} />);
+	const input = component.container.querySelector('input');
+	fireEvent.change(input, { target: { value: 'val' } });
+
+	expect(input.value).toBe('val');
 });
