7e646edb75cea5499ca3451caadf12457fafb36d
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-30 22:01:54 +0200

    Utility functions separated

--- src/services/utils.js
+++ src/services/utils.js
@@ -21,6 +21,7 @@ export const formFieldsCreate = function(
 	this.touched = false;
 };
 
+//SINGLE FIELD VALIDATION CHECK
 export const fieldValidCheck = (inputValue, rules, userInfo) => {
 	let isValid = true;
 	if (rules.required) {
@@ -37,4 +38,71 @@ export const fieldValidCheck = (inputValue, rules, userInfo) => {
 	}
 
 	return isValid;
+};
+
+// setState CALLBACK
+export const fieldValidationOnID = (fields, userInfo, id) => {
+	let fieldsCopy = { ...fields };
+	console.log(id + 'field check');
+
+	let fieldChecked = fieldValidCheck(
+		userInfo[id],
+		fields[id].validation,
+		userInfo
+	);
+	fieldsCopy[id].isValid = fieldChecked;
+
+	return fieldsCopy;
+};
+
+//OVERALL FORM VALIDATION
+export const formValidation = (fields) => {
+	let formIsValid = true;
+	for (const key in fields) {
+		formIsValid = fields[key].isValid && formIsValid;
+	}
+	return formIsValid;
+};
+
+//POSTING DATA TO SERVER
+export const postData = async (userInfo) => {
+	try {
+		let response = await fetch('http://localhost:3001/usersData', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(userInfo)
+		});
+		if (response.status === 201) {
+			let data = await response.json();
+			console.log("User's data:", data);
+			alert('Your data was send successfully');
+		} else {
+			alert(
+				"We'\re sorry. Your data wasn't sent. Status: " +
+					response.statusText
+			);
+		}
+	} catch (error) {
+		console.log(error);
+		alert("We're sorry ((..  An error: " + error.message + ' occured');
+	}
+};
+
+//CLEANING INPUT FILEDS AFTER SUBMIT
+export const clearInputFields = (fields) => {
+	let fieldsCopy = { ...fields };
+	for (const key in fieldsCopy) {
+		fieldsCopy[key].value = '';
+	}
+	return fieldsCopy;
+};
+
+export const clearUserInfo = (userInfo) => {
+	let userInfoCopy = { ...userInfo };
+	for (const key in userInfoCopy) {
+		userInfoCopy[key] = '';
+	}
+	return userInfoCopy;
 };
